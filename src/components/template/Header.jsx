697243e2c95832302aeb05e27e36586e3ea119f6
Lucas Menezes <lucasmpx2000@gmail.com>
2019-12-03 08:25:35 -0300

    [UPDATE] Improved the header

--- src/components/template/Header.jsx
+++ src/components/template/Header.jsx
@@ -2,7 +2,12 @@ import './Header.css';
 import React from 'react';
 
 export default (props) => (
-  <header className="header">
-    SAQR - Header
+  <header className="header d-nome d-sm-flex flex-column">
+    <h1 className="mt-3">
+      <i className={`fa fa-${props.icon}`} />
+      {' '}
+      {props.title}
+    </h1>
+    <p className="lead text-muted">{props.subtitle}</p>
   </header>
 );
--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,5 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 import React from 'react';
+
+// My Components
 import Logo from '../components/template/Logo';
 import Main from '../components/template/Main';
 import Nav from '../components/template/Nav';
@@ -10,7 +14,11 @@ function App() {
     <div className="App">
       <Logo />
       <Nav />
-      <Main />
+      <Main
+        icon="home"
+        title="Home"
+        subtitle="SAQR System's Main Page"
+      />
       <Footer />
     </div>
   );
