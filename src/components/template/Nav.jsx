0874f54532fec87272f847f17f715b8999d4e73b
Lucas Menezes <lucasmpx2000@gmail.com>
2019-12-03 08:51:48 -0300

    [UPDATE] Improved the nav bar

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -2,7 +2,28 @@ import './Nav.css';
 import React from 'react';
 
 export default (props) => (
-  <aside className="Nav">
-    SAQR - Nav
+  <aside className="menu-area">
+    <nav className="menu">
+      <a href="#/">
+        <i className="fa fa-home" />
+        {' '}
+        Home
+      </a>
+      <a href="#/simulate">
+        <i className="fa fa-calculator" aria-hidden="true" />
+        {' '}
+        Simulate
+      </a>
+      <a href="#/graphs">
+        <i className="fa fa-area-chart" aria-hidden="true" />
+        {' '}
+        Graphs
+      </a>
+      <a href="#/previous_simulations">
+        <i className="fa fa-database" aria-hidden="true" />
+        {' '}
+        Previous Simulations
+      </a>
+    </nav>
   </aside>
 );
